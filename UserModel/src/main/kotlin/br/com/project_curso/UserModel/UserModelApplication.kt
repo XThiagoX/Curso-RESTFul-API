b161e8e713a8dd83dff4ae8e33779ba6bd7b72b5
Thiago Araujo <thiagoaraujo43034@gmail.com>
2025-04-10 22:09:00 -0300

    <fix>[UserModelApplication]: Remoção linha para exclusão de config. automatica do banco de dados. Comando não permitia inicialização do banco

--- UserModel/src/main/kotlin/br/com/project_curso/UserModel/UserModelApplication.kt
+++ UserModel/src/main/kotlin/br/com/project_curso/UserModel/UserModelApplication.kt
@@ -1,10 +1,16 @@
 package br.com.project_curso.UserModel
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
-@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
+@SpringBootApplication
+@EntityScan("br.com.project_curso.UserModel.model")
+@EnableJpaRepositories("br.com.project_curso.UserModel.repository")
+@ComponentScan("br.com.project_curso.UserModel")
 class UserModelApplication
 
 fun main(args: Array<String>) {
