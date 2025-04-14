48cbf31ea01ad0ab16e1e874aeaa86317da507ab
Thiago Araujo <thiagoaraujo43034@gmail.com>
2025-04-13 23:08:24 -0300

    refactor(UserController): simplifica endpoints com anotações específicas

--- UserModel/src/main/kotlin/br/com/project_curso/UserModel/controller/UserController.kt
+++ UserModel/src/main/kotlin/br/com/project_curso/UserModel/controller/UserController.kt
@@ -4,7 +4,12 @@ import br.com.project_curso.UserModel.model.User
 import br.com.project_curso.UserModel.services.UserServices
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
@@ -18,34 +23,36 @@ class UserController {
     @Autowired
     private lateinit var userService : UserServices
 
-    @RequestMapping(value = ["/{id}"], method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping( "/{id}",
+        produces = [MediaType.APPLICATION_JSON_VALUE])
     fun findUser(@PathVariable(value = "id") id : Long): User{
         return userService.findById(id)
     }
 
-    @RequestMapping(value = ["/"], method = [RequestMethod.POST],
+    @GetMapping(value = ["/"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun findAllUsers(): List<User>{
+        return userService.findAll()
+    }
+
+    @PostMapping(
         consumes = [MediaType.APPLICATION_JSON_VALUE],
         produces = [MediaType.APPLICATION_JSON_VALUE])
     fun create(@RequestBody user: User): User{
         return userService.create(user)
     }
 
-    @RequestMapping(value = ["/{id}"], method = [RequestMethod.PUT],
+    @PutMapping(value = ["/{id}"],
         consumes = [MediaType.APPLICATION_JSON_VALUE],
         produces = [MediaType.APPLICATION_JSON_VALUE])
     fun update(@RequestBody user: User): User{
         return userService.update(user)
     }
 
-    @RequestMapping(value = ["/{id}"], method = [RequestMethod.DELETE],
+    @DeleteMapping(value = ["/{id}"],
         consumes = [MediaType.APPLICATION_JSON_VALUE],
         produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun delete(@PathVariable(value = "id") id: Long){
+    fun delete(@PathVariable(value = "id") id: Long): ResponseEntity<*>{
         userService.delete(id)
-    }
-
-    @RequestMapping(value = ["/"], method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun findAllUsers(): List<User>{
-        return userService.findAll()
+        return ResponseEntity.noContent().build<Any>()
     }
 }
