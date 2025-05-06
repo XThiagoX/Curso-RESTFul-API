cf6275617d0d5bdfa524be3f8e2f2077cf34bf1f
Thiago Araujo <thiagoaraujo43034@gmail.com>
2025-05-05 23:25:52 -0300

    fix(Repository): Correção do tipo de entida do repositório deo UserVO para User. Entidade real correta

--- UserModel/src/main/kotlin/br/com/project_curso/UserModel/repository/UserRepository.kt
+++ UserModel/src/main/kotlin/br/com/project_curso/UserModel/repository/UserRepository.kt
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UserRepository: JpaRepository<UserVO, Long> {
+interface UserRepository: JpaRepository<User, Long> {
 
 }
