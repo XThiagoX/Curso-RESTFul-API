db34c35911767a524ae6f803697db59237ba61cc
Thiago Araujo <thiagoaraujo43034@gmail.com>
2025-05-13 22:50:17 -0300

    feat(Versionamento de API - Service): Criação de método de conversão de Usuário

--- UserModel/src/main/kotlin/br/com/project_curso/UserModel/services/UserServices.kt
+++ UserModel/src/main/kotlin/br/com/project_curso/UserModel/services/UserServices.kt
@@ -1,17 +1,24 @@
 package br.com.project_curso.UserModel.services
 
 import br.com.project_curso.UserModel.data.vo.v1.UserVO
+import br.com.project_curso.UserModel.data.vo.v2.UserVO as UserVersion2
 import br.com.project_curso.UserModel.exceptions.ResourceNotFoundException
 import br.com.project_curso.UserModel.mapper.DozerMapper
+import br.com.project_curso.UserModel.mapper.custom.UserMapper
 import br.com.project_curso.UserModel.model.User
 import br.com.project_curso.UserModel.repository.UserRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.util.logging.Logger
 
 @Service
 class UserServices(
-    private val repository: UserRepository
+    private val repository: UserRepository,
+    //private val customUserMapper : UserMapper
 ) {
+
+    @Autowired
+    private lateinit var customUserMapper : UserMapper
 
     private val logger = Logger.getLogger(UserServices::class.java.name)
 
@@ -32,6 +39,12 @@ class UserServices(
         logger.info("Creating one person with name ${user.firstName}")
         var entity : User = DozerMapper.parseObject(user, User::class.java)
         return DozerMapper.parseObject(repository.save(entity), UserVO::class.java)
+    }
+
+    fun createVersion2(user: UserVersion2): UserVersion2 {
+        logger.info("Creating one person with name ${user.firstName}")
+        var entity : User = customUserMapper.mapVOToEntity(user)
+        return customUserMapper.mapEntityToVO(repository.save(entity))
     }
 
     fun update(user: UserVO): UserVO {
