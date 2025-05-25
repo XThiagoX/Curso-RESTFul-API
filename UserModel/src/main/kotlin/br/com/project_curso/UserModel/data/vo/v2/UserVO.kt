fd2997d04d7e935ce9a5f946439df68eb11d0c06
Thiago Araujo <thiagoaraujo43034@gmail.com>
2025-05-25 15:41:30 -0300

    feature: Serialização do userVO2

--- UserModel/src/main/kotlin/br/com/project_curso/UserModel/data/vo/v2/UserVO.kt
+++ UserModel/src/main/kotlin/br/com/project_curso/UserModel/data/vo/v2/UserVO.kt
@@ -1,12 +1,17 @@
 package br.com.project_curso.UserModel.data.vo.v2
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import java.util.Date
 
-
+@JsonPropertyOrder("id", "firstName", "lastName", "gender", "birthDate", "address") //Serialização: muda a ordem
 data class UserVO (
 
     var id: Long = 0,
+    @field:JsonProperty("first_Name") //Serialização: muda nome na api
     var firstName : String? = "",
+    @field:JsonProperty("last_Name")
     var lastName : String? = "",
     var birthday : Date? = null,
     var address : String? = "",
