b24040e2a572895bbb7b71aa1109d600ef25287a
Thiago Araujo <thiagoaraujo43034@gmail.com>
2025-06-13 21:49:49 -0300

    feat(Hateoas): implementação de link refenciando o próprio objeto para o userVO na busca por id

--- UserModel/src/main/kotlin/br/com/project_curso/UserModel/data/vo/v1/UserVO.kt
+++ UserModel/src/main/kotlin/br/com/project_curso/UserModel/data/vo/v1/UserVO.kt
@@ -1,11 +1,15 @@
 package br.com.project_curso.UserModel.data.vo.v1
 
+import com.github.dozermapper.core.Mapping
+import org.springframework.hateoas.RepresentationModel
+
 
 data class UserVO (
 
-    var id: Long = 0,
+    @Mapping("id")
+    var key: Long = 0,
     var firstName : String? = "",
     var lastName : String? = "",
     var address : String? = "",
     var gender : String? = ""
-)
+) : RepresentationModel<UserVO>()
--- UserModel/src/main/kotlin/br/com/project_curso/UserModel/services/UserServices.kt
+++ UserModel/src/main/kotlin/br/com/project_curso/UserModel/services/UserServices.kt
@@ -1,5 +1,6 @@
 package br.com.project_curso.UserModel.services
 
+import br.com.project_curso.UserModel.controller.UserController
 import br.com.project_curso.UserModel.data.vo.v1.UserVO
 import br.com.project_curso.UserModel.data.vo.v2.UserVO as UserVersion2
 import br.com.project_curso.UserModel.exceptions.ResourceNotFoundException
@@ -8,6 +9,7 @@ import br.com.project_curso.UserModel.mapper.custom.UserMapper
 import br.com.project_curso.UserModel.model.User
 import br.com.project_curso.UserModel.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo
 import org.springframework.stereotype.Service
 import java.util.logging.Logger
 
@@ -26,7 +28,10 @@ class UserServices(
         logger.info("Finding one user")
         val users = repository.findById(id)
             .orElseThrow { ResourceNotFoundException("No Records found for this ID!") }
-        return DozerMapper.parseObject( users, UserVO::class.java)
+        val userVO: UserVO = DozerMapper.parseObject( users, UserVO::class.java)
+        val withSelfRel = linkTo(UserController::class.java).slash(userVO.key).withSelfRel()
+        userVO.add(withSelfRel)
+        return userVO
     }
 
     fun findAll(): List<UserVO> {
@@ -48,8 +53,8 @@ class UserServices(
     }
 
     fun update(user: UserVO): UserVO {
-        logger.info("Update one person with id: ${user.id}")
-        val entity = repository.findById(user.id!!)
+        logger.info("Update one person with id: ${user.key}")
+        val entity = repository.findById(user.key!!)
             .orElseThrow { ResourceNotFoundException("No Records found for this ID!") }
 
         entity.firstName = user.firstName
